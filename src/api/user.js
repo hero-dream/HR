5a72ed3467c4aac5a9bf20897c3c18d7def91ec3
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-10 10:58:57 +0800

    开发环境的基础地址和代理对应

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 export function login(data) {
   return request({
     method: 'POST',
-    url: '/api/sys/login',
+    url: '/sys/login',
     data: {
       mobile: '13800000002',
       password: '123456' }
