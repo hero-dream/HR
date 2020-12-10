1d5e13871c30aa5cd8d66d52e0ee9a4f50dabb2d
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-10 10:56:04 +0800

    在request中设置baseUrl

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,11 @@
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import axios from 'axios'
-const service = axios.create() // 创建一个axios的实例
+const service = axios.create({
+// 如果执行 npm run dev  值为 /api 正确  /api 这个代理只是给开发环境配置的代理
+  // 如果执行 npm run build 值为 /prod-api  没关系  运维应该在上线的时候 给你配置上 /prod-api的代理
+  baseURL: process.env.VUE_APP_BASE_API, // 设置axios请求的基础的基础地址
+  timeout: 5000 // 定义5秒超时
+}) // 创建一个axios的实例
 service.interceptors.request.use() // 请求拦截器
 service.interceptors.response.use() // 响应拦截器
 export default service // 导出axios实例
