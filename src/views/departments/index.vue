1249b0572c6dc5c3711b0f6bff27a949f19448d8
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-13 19:56:42 +0800

    写死数据的树形图

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,47 +1,97 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组件架构
-        <el-tree :data="myTree" :props="options" :default-expand-all="true" />
-      </h2>
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
+          <el-col>
+            <span style=" font-weight: 700;">江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 下拉菜单 element -->
+                <el-dropdown>
+                  <span>
+                    操作<i class="el-icon-arrow-down" />
+                  </span>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
+          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
+          <el-row slot-scope="scope" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+            <el-col>
+              <!-- 左侧内容 -->
+              <span>{{ scope. data.name }}</span>
+            </el-col>
+            <el-col :span="4">
+              <el-row type="flex" justify="end">
+                <el-col>{{ scope. data.manager }}</el-col>
+                <el-col>
+                  <!-- 放置下拉菜单 -->
+                  <el-dropdown>
+                    <!-- 内容 -->
+                    <span>操作
+                      <i class="el-icon-arrow-down" />
+                    </span>
+                    <!-- 具名插槽 -->
+                    <el-dropdown-menu slot="dropdown">
+                      <!-- 下拉选项 -->
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                      <el-dropdown-item>编辑部门</el-dropdown-item>
+                      <el-dropdown-item>删除部门</el-dropdown-item>
+
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+
+            <!-- 右侧内容 -->
+            </el-col>
+          </el-row>        </el-tree>
+      </el-card>
+
     </div>
   </div>
 </template>
 
 <script>
+import { getTments } from '@/api/departments'
 export default {
   data() {
     return {
-      myTree: [
-        {
-          label: '一级菜单',
-          children: [
-            { label: '二级菜单1' },
-            {
-              label: '二级菜单',
-
-              children: [{
-                label: '三级菜单'
-              }]
-            }]
-        },
-        { label: '一级菜单2' }
-      ],
-      options: {
-        // 这一个是树形结构的描述
-        // 对于一个属性结构来说, 每个节点都有两个重要的属性
-        // 1. 当前节点名字段 如 mingzi
-        // 2. 当前节点的下级菜单字段名 如 mySon
-        // 这里这个配置就是为了告诉组件, 用哪个字段作为名称, 又用哪个字段作为子级菜单
-        label: 'label',
-        children: 'children'
+      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }],
+      defaultProps: {
+        label: 'name' // 表示 从这个属性显示内容
       }
+
     }
+  },
+  created() {
+    getTments().then(res => {
+      console.log(res)
+    })
   }
 }
 </script>
 
 <style>
+.tree-card {
 
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
