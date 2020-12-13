385d494b5b352404b95c529782d9f3c61ee02412
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-13 19:58:16 +0800

    userId的添加

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,7 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
-  name: state => state.user.UserInfo.username
+  name: state => state.user.UserInfo.username,
+  userId: state => state.user.UserInfo.userId
 }
 export default getters
