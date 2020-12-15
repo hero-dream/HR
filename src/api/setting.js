8fe1416b4417163add16fef2164ec1edcd58c1b5
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-15 14:20:28 +0800

    封装新增功能的请求

--- src/api/setting.js
+++ src/api/setting.js
@@ -8,11 +8,21 @@ export function getRoleList(params) {
     // params是查询参数，里面需要携带分页信息
   })
 }
-
+// 删除
 export function delRole(roleId) {
   return request({
     url: '/sys/role/' + roleId,
     method: 'delete'
+  })
+}
+
+// 新增
+export function addRole(data) {
+  return request({
+    url: '/sys/role',
+    method: 'post',
+    data
+
   })
 }
 
