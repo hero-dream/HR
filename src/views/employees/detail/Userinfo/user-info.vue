890392f8fd8ea79c2eb2870b44058e1422805677
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-19 21:20:05 +0800

    个人信息的数据回显

--- src/views/employees/detail/Userinfo/user-info.vue
+++ src/views/employees/detail/Userinfo/user-info.vue
@@ -284,7 +284,8 @@
 
 <script>
 import EmployeeEnum from '@/api/constant/employees'
-
+import { getPersonalDetail, updatePersonal, saveUserDetailById } from '@/api/employees'
+import { getUserDetailById } from '@/api/user'
 export default {
   data() {
     return {
@@ -356,9 +357,26 @@ export default {
       }
     }
   },
+  created() {
+    this.getPersonalDetail()
+    this.getUserDetailById()
+  },
   methods: {
-    saveUser() {},
-    savePersonal() {}
+    async getPersonalDetail() {
+      this.formData = await getPersonalDetail(this.userId) // 获取员工数据
+    },
+    async savePersonal() {
+      await updatePersonal({ ...this.formData, id: this.userId })
+      this.$message.success('保存成功')
+    },
+    async saveUser() {
+    //  调用父组件
+      await saveUserDetailById(this.userInfo)
+      this.$message.success('保存成功')
+    },
+    async getUserDetailById() {
+      this.userInfo = await getUserDetailById(this.userId) // 获取id
+    }
   }
 }
 </script>
