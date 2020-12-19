9e0b94e50cb454388fa9b794d55c15328efe6988
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-19 21:19:02 +0800

    对导入功能的时间进行修正

--- src/views/imoort/index.vue
+++ src/views/imoort/index.vue
@@ -25,19 +25,32 @@ export default {
         return this.cnToEn(item, userRelations)
         // console.log(item)
       })
-
       await importEmployee(data)
       this.$message.success('导入员工成功')
       this.$router.push('employees')
-
       console.log(data)
+    },
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)
     },
     // 遍历key值
     cnToEn(item, userRelations) {
       const newItem = {}
       for (const key in item) {
         const newKey = userRelations[key] // newKey是英文的 userRelations[key]是中文 这里为头部的
-        const value = item[key] // 这是值的输入
+        let value = item[key] // 这是值的输入
+        if (newKey === 'timeOfEntry' || newKey === 'correctionTime') {
+          value = new Date(this.formatDate(value, '/'))
+          console.log(value)
+        }
         newItem[newKey] = value
       }
       return newItem
