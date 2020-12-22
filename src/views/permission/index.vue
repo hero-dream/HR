402f1a6112df42b832a79213e2baed1e09b11c0b
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-22 15:35:41 +0800

    权限管理树形结构成型

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,39 +1,54 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <template>
-        <div class="dashboard-container">
-          <div class="app-container">
-            <!-- 靠右的按钮 -->
-            <page-tools>
-              <template v-slot:after>
-                <el-button type="primary" size="small">添加权限</el-button>
-              </template>
-            </page-tools>
-            <!-- 表格 -->
-            <el-table border>
-              <el-table-column align="center" label="名称" />
-              <el-table-column align="center" label="标识" />
-              <el-table-column align="center" label="描述" />
-              <el-table-column align="center" label="操作">
-                <template>
-                  <el-button type="text">添加</el-button>
-                  <el-button type="text">编辑</el-button>
-                  <el-button type="text">删除</el-button>
-                </template>
-              </el-table-column>
-
-            </el-table>
-          </div>
-        </div>
-      </template>
+      <h2>
+        权限
+      </h2>
+      <!-- 头部工具 -->
+      <PageTools>
+        <template slot="after">
+          <el-button type="primary">新增权限</el-button>
+        </template>
+      </PageTools>
+      <!-- 主要内容 -->
+      <el-card>
+        <el-table border :data="permissionList" row-key="id">
+          <el-table-column label="权限名称" prop="name" width="160px" />
+          <el-table-column align="center" label="权限标识" prop="code" />
+          <el-table-column align="center" label="权限描述" prop="description" />
+          <el-table-column align="center" label="操作">
+            <template>
+              <el-button type="text">添加</el-button>
+              <el-button type="text">编辑</el-button>
+              <el-button type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permisson'
+import { tranListToTreeData } from '@/utils'
 export default {
 
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      const data = await getPermissionList()
+      console.log(data)
+      this.permissionList = tranListToTreeData(data, '0')
+    }
+  }
 }
 </script>
 
