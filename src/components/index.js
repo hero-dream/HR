dd33941860256eaa89777115f81dcc69577ac568
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-23 17:59:16 +0800

    封装全局的分配权限弹框

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 // import UploadExcel from '@/components/UploadExcel'
 import ImageUpload from './ImageUpload'
+import SettingView from './setting'
 export default {
   install(Vue) {
     //  注册全局的通用栏组件对象
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
     Vue.component('ImageUpload', ImageUpload)
+    Vue.component('SettingView', SettingView) // 分配权限弹框
   }
 }
