28acf6f20a8730329a626465d07ba476cd560385
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-23 17:50:23 +0800

    封装请求 给角色分配权限

--- src/api/setting.js
+++ src/api/setting.js
@@ -49,3 +49,11 @@ export function getCompanyInfo(companyId) {
   })
 }
 
+// 给角色分配权限
+export function assignPerm(data) {
+  return request({
+    url: '/sys/role/assignPrem',
+    method: 'put',
+    data
+  })
+}
