0856eefbbdd39948ba133198d31ee17b25474830
weixiaoshan@qq.com <weixiaoshan@qq.com>
2020-12-24 16:23:41 +0800

    06-将原来写死的动态路由合并, 移动到路由守卫获取用户信息之后

--- src/permission.js
+++ src/permission.js
@@ -1,18 +1,29 @@
 import router from '@/router'
 import store from '@/store'
+
 import NProgress from 'nprogress' // 引入一份进度条插件
 import 'nprogress/nprogress.css' // 引入进度条样式
+import { asyncRoutes } from '@/router'
 
 const whileList = ['/login', '/404'] // 设置白名单
 
 // 前置守卫
-router.beforeEach((to, from, next) => {
+router.beforeEach(async(to, from, next) => { // 函数(to, from, next)
   NProgress.start() // 开启进度条
   if (store.getters.token) { // 如果有token
     if (to.path === '/login') { // 在登录页登录
       next('/') // 就跳转往首页
     } else {
-      next() // 如果没有就放行
+      if (!store.getters.userId) {
+        await store.dispatch('user/getUserInfo') // 获取用户信息，异步
+        router.addRoutes(asyncRoutes) // 添加动态路由
+        next(to.path) // 如果是添加路由，由于vue-router的缺陷，需要在一个地址跳转一次才行
+      // }
+      // 如果是添加路由，由于vue-router的缺陷，需要在一个地址跳转一次才行
+      // next(to.path) // 没有else会死循环
+      } else {
+        next() // 如果没有路由就放行
+      }
     }
   } else {
     if (whileList.indexOf(to.path) > -1) { // 当是白名单时
